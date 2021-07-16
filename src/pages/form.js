510d36f2ea0efb1fd41e0ae59548706cc4902dac
JX23 <joaquim.tiago1999@gmail.com>
2021-07-16 16:25:26 +0100

    test-form-upload

--- src/pages/form.js
+++ src/pages/form.js
@@ -2,32 +2,51 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import {Container} from "design-react-kit";
 
 const Form = () => (
   <Layout>
     {/* eslint-disable-next-line*/}
     <SEO title="form" />
-    <form name="contact" action="/success" method="POST" data-netlify="true">
-    <input type="hidden" name="bot-field" />
+    <Container>
+          <form name="contact" action="/success" method="POST" data-netlify="true">
+          <input type="hidden" name="bot-field" />
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>   
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    <br/>
+    <form name="dogPictures" method="POST" data-netlify="true">
   <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
+    <label>Dog’s Name: <input type="text" name="name" /></label>   
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
+    <label>Dog’s Picture: <input type="file" name="picture" /></label>
   </p>
   <p>
     <button type="submit">Send</button>
   </p>
 </form>
+
+    </Container>
+
+
+
 
   </Layout>
 );
